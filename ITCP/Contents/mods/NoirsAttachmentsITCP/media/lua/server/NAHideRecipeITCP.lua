c0dff8076fd93e4befdb83d2ff544251ca68821a
opariffazman <ariff.azman@proton.me>
2023-08-14 17:25:48 +0800

    feat: initial commit

---
+++ ITCP/Contents/mods/NoirsAttachmentsITCP/media/lua/server/NAHideRecipeITCP.lua
@@ -0,0 +1,43 @@
+if getActivatedMods():contains("nattachments") then
+  require("NATT_Recipes");
+else
+  return
+end
+
+local recipes = getAllRecipes()
+
+local function HideRecipe()
+  if recipes then
+    for i = 0, recipes:size() - 1 do
+      local recipe = recipes:get(i)
+      print("nattachments patched");
+
+      if recipe:getName() == "Add Weapon Slot"
+          or recipe:getName() == "Remove Weapon Slot"
+          or recipe:getName() == "Add Short Weapon Slot"
+          or recipe:getName() == "Remove Short Weapon Slot"
+          or recipe:getName() == "Add Flashlight Slot"
+          or recipe:getName() == "Remove Flashlight Slot"
+          or recipe:getName() == "Add Left Slot"
+          or recipe:getName() == "Remove Left Slot"
+          or recipe:getName() == "Add Right Slot"
+          or recipe:getName() == "Remove Right Slot"
+          or recipe:getName() == "Add Utility Left Slot"
+          or recipe:getName() == "Remove Utility Left Slot"
+          or recipe:getName() == "Add Utility Right Slot"
+          or recipe:getName() == "Remove Utility Right Slot"
+          or recipe:getName() == "Add Container Slot"
+          or recipe:getName() == "Remove Container Slot"
+          or recipe:getName() == "Add Small Container Left Slot"
+          or recipe:getName() == "Remove Small Container Left Slot"
+          or recipe:getName() == "Add Small Container Right Slot"
+          or recipe:getName() == "Remove Small Container Right Slot"
+          or recipe:getName() == "Add Bedroll Slot"
+          or recipe:getName() == "Remove Bedroll Slot" then
+        recipe:setIsHidden(true)
+      end
+    end
+  end
+end
+
+Events.OnGameStart.Add(HideRecipe);
