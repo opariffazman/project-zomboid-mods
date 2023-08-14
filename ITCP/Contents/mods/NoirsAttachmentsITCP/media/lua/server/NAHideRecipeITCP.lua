1724d60a21cc22fa0b099cf4d6a5a1d63a834de0
opariffazman <ariff.azman@proton.me>
2023-08-14 17:45:47 +0800

    fix: modified correct naming for slot attachment, commented out debug message

--- ITCP/Contents/mods/NoirsAttachmentsITCP/media/lua/server/NAHideRecipeITCP.lua
+++ ITCP/Contents/mods/NoirsAttachmentsITCP/media/lua/server/NAHideRecipeITCP.lua
@@ -10,7 +10,7 @@ local function HideRecipe()
   if recipes then
     for i = 0, recipes:size() - 1 do
       local recipe = recipes:get(i)
-      print("nattachments patched");
+      -- print("nattachments patched")
 
       if recipe:getName() == "Add Weapon Slot"
           or recipe:getName() == "Remove Weapon Slot"
@@ -18,10 +18,10 @@ local function HideRecipe()
           or recipe:getName() == "Remove Short Weapon Slot"
           or recipe:getName() == "Add Flashlight Slot"
           or recipe:getName() == "Remove Flashlight Slot"
-          or recipe:getName() == "Add Left Slot"
-          or recipe:getName() == "Remove Left Slot"
-          or recipe:getName() == "Add Right Slot"
-          or recipe:getName() == "Remove Right Slot"
+          or recipe:getName() == "Add Left Side Slot"
+          or recipe:getName() == "Remove Left Side Slot"
+          or recipe:getName() == "Add Right Side Slot"
+          or recipe:getName() == "Remove Right Side Slot"
           or recipe:getName() == "Add Utility Left Slot"
           or recipe:getName() == "Remove Utility Left Slot"
           or recipe:getName() == "Add Utility Right Slot"
