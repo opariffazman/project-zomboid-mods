0f5a69ae57dc5147d3e5708bfa7436827fffba9d
opariffazman <ariff.azman@proton.me>
2023-08-21 19:16:33 +0800

    feat: initial commit

---
+++ ITCP/Contents/mods/CraftingEnhancedCoreITCP/media/lua/client/CraftingEnhancedCore/TableBuildMenuITCP.lua
@@ -0,0 +1,34 @@
+local function predicateNotBroken(item)
+  return not item:isBroken()
+end
+
+local original_canBuildObject
+
+local function new_canBuildObject(_tooltip, player, table)
+  local inv = getPlayer(player):getInventory()
+
+  if not inv:getFirstTagEval(table.requireTool, predicateNotBroken) then
+    _tooltip.description = _tooltip.description .. ' <RGB:1,0,0>' ..
+        getText("ContextMenu_RequireTool") .. " " .. getItemNameFromFullType("Base." .. table.requireTool) .. ' <LINE>'
+  end
+
+  for _, material in pairs(table.recipe) do
+    local invItemCount = inv:getItemCountFromTypeRecurse(material.type)
+
+    if invItemCount >= material.amount or ISBuildMenu.cheat then
+      _tooltip.description = _tooltip.description ..
+          ' <RGB:1,1,1>' ..
+          getItemNameFromFullType(material.type) .. ' ' .. material.amount .. '/' .. material.amount .. ' <LINE>'
+    else
+      _tooltip.description = _tooltip.description ..
+          ' <RGB:1,0,0>' ..
+          getItemNameFromFullType(material.type) .. ' ' .. invItemCount .. '/' .. material.amount .. ' <LINE>'
+    end
+  end
+end
+
+Events.OnGameBoot.Add(function()
+  original_canBuildObject = CraftingEnhancedCore.canBuildObject
+
+  CraftingEnhancedCore.canBuildObject = new_canBuildObject
+end)
